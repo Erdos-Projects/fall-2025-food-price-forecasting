d438a21b0ff2e5c49e63603a527c74929c5e65a6
tai-bennett <bennettduncantai@gmail.com>
2025-10-17 16:57:43 -0700

    eval

--- src/eval/evaluation_plan.md
+++ src/eval/evaluation_plan.md
@@ -2,7 +2,14 @@
 ## Unit of Analysis
 Each row of the data is a vector of CPI values for a certain year-month. 
 ## Split Strategy
-- Sliding window split
-- Sliding window with gap split
-- [Combinatorial Purged Cross Validation](https://towardsai.net/p/l/the-combinatorial-purged-cross-validation-method)
-## Planned Stress Tests
+We plan to evaluate model performance on forecasting the last $n=5$ years one year at a time. That is, if data is provided up through 2025-07 then splits take the form
+- split_0_train : date <= 2020-07
+- split_0_test : 2020-07 < date <= 2021-07
+- split_1_train : date <= 2021-07
+- split_1_test : 2021-07 < date <= 2022-07
+- ...
+- split_4_train : date <= 2024-07
+- split_4_test : 2024-07 < date <= 2025-07
+*Maybe add visual to illustrate splits*
+
+The motiviation for these evaluation splits comes from the contextual motivation for forecasting. As a policy maker, you are forecasting and making predictions for the duration of your term to inform the policies you will propose and implement. Thus, forecasting in the 1-4 year range is desired with 1 year being more reliable than longer forecasts. 
--- src/splits/n_year_splitter.py
+++ src/splits/n_year_splitter.py
@@ -1,34 +1,49 @@
 import pandas as pd
-import pdb
+import pdb, os
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 def n_year_splitter(df, n=5, save_path=None):
     """
-    returns a generator object that produces training splits for
+    returns splits for
     the last n year. That is, if 2025-07-01 is the max date and n = 2
     then the training splits will be
     train1 = dates <= 2023-07-01
     test1 = 2023-07-01 < dates <= 2024-07-01
     train2 = dates <= 2024-07-01
     test2 = 2024-07-01 < dates <= 2025-07-01
+
+    if save_path is None, then function returns a generator object
+    else it saves all data as csv files
     """
     df.index = pd.to_datetime(df.index)
     #final_date = datetime.strptime(df.index.max(), "%Y-%m-%d")
     final_date = df.index.max()
     dt = final_date - relativedelta(years=n+1)
+    train = []
+    test = []
     for i in range(n):
         dt = dt + relativedelta(years=1)
         test_mask = (df.index > dt) & (df.index <= (dt + relativedelta(years=1)))
         test_indices = df.index[test_mask]
         train_mask = df.index <= dt
         train_indices = df.index[train_mask]
-        yield train_indices, test_indices
-        
+        if save_path is None:
+            train.append(train_indices)
+            test.append(test_indices)
+        else:
+            train_name = os.path.join(save_path, f"split_{i}_train.csv")
+            test_name = os.path.join(save_path, f"split_{i}_test.csv")
+            df.loc[train_indices].to_csv(train_name)
+            df.loc[test_indices].to_csv(test_name)
+    if save_path is None:
+        return train, test
 
 
 if __name__ == "__main__":
-    data_path = "../data/processed/CPI.csv"
-    df = pd.read_csv(data_path, index_col='date')
-    g = n_year_splitter(df, n=5)
+    data_path = "../data/processed/"
+    data_name = "CPI.csv"
+    path = os.path.join(data_path, data_name)
+    df = pd.read_csv(path, index_col='date')
+    n_year_splitter(df, n=5, save_path=data_path)
     pdb.set_trace()
